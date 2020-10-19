37a562fa763c28a4e064cd21607066b70ec1d020
Cheolhoon Choi <54978134+cheolhoon@users.noreply.github.com>
2020-10-19 02:48:59 -1000

    Section 5

--- app/imports/ui/Task.js
+++ app/imports/ui/Task.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Task = ({ task, onCheckboxClick, onDeleteClick }) => {
   return (
-      <li>
+      <li classname='task'>
         <input
             type="checkbox"
             checked={!!task.isChecked}
@@ -10,7 +10,7 @@ export const Task = ({ task, onCheckboxClick, onDeleteClick }) => {
             readOnly
         />
         <span>{task.text}</span>
-        <button onClick={ () => onDeleteClick(task) }>&times;</button>
+        <button onClick={() => onDeleteClick(task)}>&times;</button>
       </li>
   );
 };
